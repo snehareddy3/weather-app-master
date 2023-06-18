209f8a94395266e6bb8ecebe9f7b874a71651af4
snehareddy3 <snehareddykancharla@gmail.com>
2023-06-18 12:05:48 -0500

    commit changes

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -10,7 +9,7 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
-  devServer: { contentBase: path.join(__dirname, "src") },
+  devServer: { static: path.join(__dirname, "src") },
   module: {
     rules: [
       {
@@ -55,7 +54,4 @@ module.exports = {
       chunkFilename: "[id].[hash].css",
     }),
   ],
-  optimization: {
-    minimizer: [new UglifyJsPlugin()],
-  },
 };
